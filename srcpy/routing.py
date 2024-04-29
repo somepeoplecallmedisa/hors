c9e35e60f5f92eedfcf409a7cf3cdcc4f13d6f62
sillygabe <gabrielonline630@gmail.com>
2024-04-29 12:51:34 +0300

    Added the ability to kill the process

--- srcpy/routing.py
+++ srcpy/routing.py
@@ -1,5 +1,7 @@
 import fastapi
 from database import Database
+import os
+from signal import SIGINT
 
 class Router():
     def __init__(self):
@@ -7,6 +9,7 @@ class Router():
         self.db = Database()
 
         self.router.add_api_route("/local/debug", self.debug)
+        self.router.add_api_route("/local/kill", self.kill)
 
     async def debug(self, req: fastapi.Request):
         if not req.client:
@@ -21,3 +24,11 @@ class Router():
             return_dict[key] = self.db[key]
 
         return return_dict
+
+    async def kill(self, req: fastapi.Request):
+        if not req.client:
+            return {"detail" : "not allowed"}
+        if not req.client.host == "127.0.0.1":
+            return {"detail" : "not allowed"}
+
+        os.kill(os.getpid(), SIGINT)
